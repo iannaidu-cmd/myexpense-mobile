bfbfb2ca24ea188c748cc41d5fc03103a9e09baf
Ian Naidu <ian.naidu@gmail.com>
2026-03-27 10:46:50 +0200

    Move Supabase credentials to env vars

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -2,9 +2,8 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
 import { Platform } from "react-native";
 
-const supabaseUrl = "https://hhfbbbxgmovfpaziebsw.supabase.co";
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhoZmJiYnhnbW92ZnBhemllYnN3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE1ODA2NTAsImV4cCI6MjA4NzE1NjY1MH0.Z2frUvYeIl7aJGsn4LG5Pm1UocBIKnx7ld80uiEGzRc";
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
